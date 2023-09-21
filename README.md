64c19cb300fcc87a21a353d9ced06869f5cabd26
JShoon <jsh021813@gmail.com>
2023-09-21 16:36:38 +0900

    Feat: 신규 기능 추가 - antd 추가 - readme 추가

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 1. 구동 환경 설치
 
-- npm create vite@latest my-vue-app -- --template react-ts
-  cd (폴더 명)
-  npm install
-  npm run dev
+   > > npm create vite@latest my-vue-app -- --template react-ts
+   > > cd (폴더 명)
+   > > npm install
+   > > npm run dev
+
+2. layout 생성
+
+- header 및 login, serviceinfo, signup 컴포넌트 생성
+
+3.
--- src/Components/Board/Board.tsx
+++ src/Components/Board/Board.tsx
@@ -1,5 +1,72 @@
+import { Card, List } from "antd";
+
+//     "projectId": 0,
+//     "projectTitle": "string",
+//     "description": "string",
+//     "creatorId": "string",
+//     "recruitmentStatus": "string",
+//     "recruitmentCount": 0,
+//     "generateDate": "string",
+//     "acceptedID": "string"
+const data = [
+  {
+    title: "프로젝트 제목1",
+    content: "프로젝트 내용1",
+    readCnt: 100,
+  },
+  {
+    title: "프로젝트 제목2",
+    content: "프로젝트 내용2",
+    readCnt: 200,
+  },
+  {
+    title: "프로젝트 제목3",
+    content: "프로젝트 내용3",
+    readCnt: 300,
+  },
+  {
+    title: "프로젝트 제목4",
+    content: "프로젝트 내용4",
+    readCnt: 400,
+  },
+];
+
 const Board = () => {
-  return <div>Board</div>;
+  return (
+    <div>
+      Board
+      <div
+        style={{
+          margin: "0px 10px 0px 10px",
+        }}
+      >
+        <List
+          grid={{ gutter: 16, column: 4 }}
+          dataSource={data}
+          renderItem={(item) => (
+            <List.Item>
+              <Card
+                title={
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
+                  >
+                    <div>{item.title}</div>
+                    <div>{item.readCnt}</div>
+                  </div>
+                }
+              >
+                {item.content}
+              </Card>
+            </List.Item>
+          )}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Board;
