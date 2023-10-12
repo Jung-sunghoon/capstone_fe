bfd53c2a8c9d2b58b7910ab52a416523f277f61a
JShoon <jsh021813@gmail.com>
2023-10-12 16:14:01 +0900

    Feat: project detail 가져오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./Components/Layout/Layout";
-import SignUp from "./Components/Layout/Header/SignUp";
-import ServiceInfo from "./Components/Layout/Header/ServiceInfo";
-import Login from "./Components/Layout/Header/Login";
-import Board from "./Components/Board/Board";
-import { AuthProvider } from "./AuthContext"; // AuthProvider 추가
-import Generate from "./Components/Board/Generate";
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './Components/Layout/Layout'
+import SignUp from './Components/Layout/Header/SignUp'
+import ServiceInfo from './Components/Layout/Header/ServiceInfo'
+import Login from './Components/Layout/Header/Login'
+import Board from './Components/Board/Board'
+import ProjectDetails from './pages/Projects/ProjectDetails'
+import { AuthProvider } from './AuthContext' // AuthProvider 추가
+import Generate from './Components/Board/Generate'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <AuthProvider>
@@ -19,11 +20,12 @@ function App() {
           <Route path="/sign-up/*" element={<SignUp />} />
           <Route path="/service-info/*" element={<ServiceInfo />} />
           <Route path="/board/*" element={<Board />} />
+          <Route path="/project/*" element={<ProjectDetails />} />
           <Route path="/generate/*" element={<Generate />} />
         </Routes>
       </Layout>
     </AuthProvider>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Projects/ProjectDetails.tsx
@@ -0,0 +1,7 @@
+export interface ProjectDetails {}
+
+const ProjectDetails: React.FC<ProjectDetails> = () => {
+  return <div>ProjectDetails</div>
+}
+
+export default ProjectDetails
