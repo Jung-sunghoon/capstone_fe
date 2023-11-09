1a341b7471da00ba8dada9c8007fee8ea875bee4
JShoon <jsh021813@gmail.com>
2023-11-09 18:21:19 +0900

    feat: 랭킹 목록 포인트 순

--- src/pages/Ranking/Ranking.tsx
+++ src/pages/Ranking/Ranking.tsx
@@ -1,4 +1,4 @@
-import { Col, Row } from 'reactstrap'
+import { Col, Row, Table } from 'reactstrap'
 import { users } from './mock/users'
 import { isEmpty, size, map } from 'lodash'
 import { Link } from 'react-router-dom'
@@ -14,80 +14,84 @@ import { Avatar, Card } from 'antd'
 const { Meta } = Card
 
 const Ranking: React.FC = () => {
+  // points를 기준으로 사용자 목록을 내림차순으로 정렬
+  //@ts-ignore
+  const sortedUsers = [...users].sort((a, b) => b.points - a.points)
+
   return (
     <div style={{ marginTop: '40px', marginLeft: '20px' }}>
       <Row>
-        {users &&
-          users.map((user: any, index: number) => {
-            // @ts-ignore
-            let img: any = images['thumbnail' + (index + 1)]
-            return (
-              <Col
-                xs="12"
-                sm="4"
-                md="3"
-                lg="2"
-                style={{
-                  marginBottom: '30px',
-                }}
-              >
-                <div className="card-container">
-                  <Card
-                    style={{
-                      maxWidth: '300px',
-                      height: '100%',
-                      position: 'relative',
-                    }}
-                    //@ts-ignore
-                    cover={<img alt="example" src={img} />}
-                    actions={[
-                      <UserOutlined key="setting" />,
-                      <ContainerOutlined key="edit" />,
-                      <EllipsisOutlined key="ellipsis" />,
-                    ]}
-                  >
-                    <Meta
-                      avatar={
-                        <Avatar
-                          style={{
-                            width: '70px',
-                            height: '70px',
-                          }}
-                          src={user.img}
-                        />
-                      }
-                      title={user.name}
-                      description={user.points}
-                    />
-                    {index < 3 ? (
-                      <div
+        {sortedUsers.map((user: any, index: number) => {
+          // @ts-ignore
+          let img: any = images['thumbnail' + (index + 1)]
+          return (
+            <Col
+              xs="12"
+              sm="4"
+              md="3"
+              lg="2"
+              style={{
+                marginBottom: '30px',
+              }}
+              key={user.id} // 사용자마다 고유한 키를 제공해야 합니다.
+            >
+              <div className="card-container">
+                <Card
+                  style={{
+                    maxWidth: '300px',
+                    height: '100%',
+                    position: 'relative',
+                  }}
+                  //@ts-ignore
+                  cover={<img alt="example" src={img} />}
+                  actions={[
+                    <UserOutlined key="setting" />,
+                    <ContainerOutlined key="edit" />,
+                    <EllipsisOutlined key="ellipsis" />,
+                  ]}
+                >
+                  <Meta
+                    avatar={
+                      <Avatar
                         style={{
-                          position: 'absolute',
-                          bottom: 135,
-                          left: 45,
+                          width: '70px',
+                          height: '70px',
                         }}
-                      >
-                        <CrownOutlined
-                          style={{
-                            color:
-                              index === 0
-                                ? 'gold'
-                                : index === 1
-                                ? 'silver'
-                                : index === 2
-                                ? '#cd7f32'
-                                : 'none',
-                            fontWeight: 'bold',
-                            fontSize: '28px',
-                          }}
-                        />
-                      </div>
-                    ) : null}
-                  </Card>
-                </div>
-              </Col>
-            )
-          })}
+                        src={user.img}
+                      />
+                    }
+                    title={user.name}
+                    description={user.points}
+                  />
+                  {index < 3 ? (
+                    <div
+                      style={{
+                        position: 'absolute',
+                        bottom: 135,
+                        left: 45,
+                      }}
+                    >
+                      <CrownOutlined
+                        style={{
+                          color:
+                            index === 0
+                              ? 'gold'
+                              : index === 1
+                              ? 'silver'
+                              : index === 2
+                              ? '#cd7f32'
+                              : 'none',
+                          fontWeight: 'bold',
+                          fontSize: '28px',
+                        }}
+                      />
+                    </div>
+                  ) : null}
+                </Card>
+              </div>
+            </Col>
+          )
+        })}
       </Row>
     </div>
   )
