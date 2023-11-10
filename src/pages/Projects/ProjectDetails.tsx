16eb8944fa496f0579249191cf02b704cf479ec7
JShoon <jsh021813@gmail.com>
2023-11-10 12:18:50 +0900

    Fix: 의미 없으므로 지움

--- src/pages/Projects/ProjectDetails.tsx
+++ src/pages/Projects/ProjectDetails.tsx
@@ -730,7 +730,6 @@ const ProjectDetails: React.FC<ProjectDetails> = () => {
 
         // comment reload
         fetchComments()
-        setCommentText('')
       } catch (error) {
         // 오류 처리
         console.error('댓글 삭제 오류:', error)
